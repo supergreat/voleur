8925f75b0c1c9b0cf6f1b10c4bb76cfb618152dd
Alex Michael <hi@alexmic.net>
2020-03-23 20:37:19 +0200

    Release 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='voleur',
-    version='0.0.1',
+    version='0.0.2',
     author='Supergreat Engineering',
     author_email='alexmic@supergreat.reviews',
     description=(
