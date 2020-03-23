00b5b7ad0591d9da0a84ce778bf9302b6f6a86d8
Alex Michael <hi@alexmic.net>
2020-03-23 21:04:56 +0200

    Add docopt in install_requires

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setuptools.setup(
     long_description_content_type='text/markdown',
     url='https://github.com/supergreat/voleur',
     packages=setuptools.find_packages(),
-    install_requires=['boto3'],
+    install_requires=['boto3', 'docopt'],
     include_package_data=True,
     classifiers=[
         'Programming Language :: Python :: 3',
