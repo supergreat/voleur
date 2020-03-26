9a270b1d5c07348e6db4c502ed12b7a649c86512
Alex Michael <hi@alexmic.net>
2020-03-26 11:57:06 +0200

    Simplify writer to stream straight into psql stdin
    The dumper now takes care of fixing sql statements.

--- voleur/writer.py
+++ voleur/writer.py
@@ -25,35 +25,10 @@ def write_dump(target: str, stream: BinaryIO):
 
     try:
         stdin = process.stdin
-        while not stream.closed:
-            stdin.write(_fix_insert_statement(stream.readline()))
+        for line in stream:
+            stdin.write(line)
         _, stderr = process.communicate()
         if process.returncode > 0:
             raise WriterError(stderr.strip().decode('utf-8'))
     finally:
         stdin.close()
-
-
-def _fix_insert_statement(line: bytes) -> bytes:
-    """Necessary hack to fix sql statements because `klepto` emits invalid SQL statements
-    for `psql`.
-
-    Args:
-        line: The line to check and fix.
-
-    Returns:
-        bytes: Fixed line.
-
-    """
-    if not line.startswith(b'INSERT INTO'):
-        return line
-
-    line = line.strip()
-    line = line.replace(b'INSERT INTO ', b'INSERT INTO public.')
-    line = line.replace(b'\'NULL\'', b'NULL')
-    line = line.replace(b'+0000 UTC', b'+00')
-
-    if not line.endswith(b';'):
-        line = line + b';'
-
-    return line + b'\n'
